5235b769a056f17d3b73a4bdee7fdb85a8bfb772
Jonathan Casson <jonathancasson@Jonathans-MacBook-Pro-2.local>
2018-05-16 12:31:40 +1000

    first commit

---
+++ js/exercise.js
@@ -0,0 +1,256 @@
+//FOR ARTSY API
+var token = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJyb2xlcyI6IiIsImV4cCI6MTUyNjk2NzU4MywiaWF0IjoxNTI2MzYyNzgzLCJhdWQiOiI1YWY0ZWFkN2EwOWE2NzZjODA4N2Y0YzAiLCJpc3MiOiJHcmF2aXR5IiwianRpIjoiNWFmYTcyOWY5YzE4ZGIyMDJlN2NjMTg5In0.zLuLtEdlB2dnD_u7VahsjNiW0RlaCu7avNn4Az_7icE"
+var clientID = "b672dc2e7f242760d0d6"
+
+var scene, camera, renderer, mesh;
+var zoom = "";
+var orbitCamera = "";
+var zooming = function(){
+	camera.position.set(camera.position.x,camera.position.y,zoom)
+	camera.updateProjectionMatrix();
+}
+//
+	document.addEventListener("keypress", function(event){
+	if(event.keyCode === 119){
+		zoom-=0.3
+	}
+	if(event.keyCode === 115){
+		console.log("down")
+		zoom+=0.2
+	}
+	zooming();
+})
+//Set up daydream variables
+if ( 'bluetooth' in navigator === false ) {
+  button.style.display = 'none';
+  message.innerHTML = 'This browser doesn\'t support the <a href="https://developer.mozilla.org/en-US/docs/Web/API/Web_Bluetooth_API" target="_blank">Web Bluetooth API</a> :(';
+}
+// document.addEventListener( 'mousemove', onDocumentMouseMove, false )
+// function onDocumentMouseMove( event ) {
+//     event.preventDefault();
+//
+//
+//
+// }
+
+	// camera.zoom = zoom
+	// camera.translateZ(zoom);
+	// console.log(camera.position.x)
+
+	// console.log(camera.zoom)
+	// zoom=""
+
+	// camera.position =
+
+
+
+function init(){
+	// Create a scene and camera
+	scene = new THREE.Scene();
+	camera = new THREE.PerspectiveCamera(90, window.innerWidth/window.innerHeight, 0.1, 1000);
+	//
+
+	// controls.update() must be called after any manual changes to the camera's transform
+	var axis = new THREE.Vector3();
+	var quaternion = new THREE.Quaternion();
+	var quaternionHome = new THREE.Quaternion();
+	var initialised = false;
+	var timeout = null;
+
+	// A Mesh is made up of a geometry and a material.
+	// Materials affect how the geometry looks, especially under lights.
+
+	const light = new THREE.PointLight("#f7f3a3");
+light.position.set(-20, 30, 0);
+console.log(light)
+light.intensity = 3;
+light.castShadow = true;
+light.shadow.mapSize.width = 2048;
+light.shadow.mapSize.height = 2048;
+scene.add(light);
+const lightHelper = new THREE.PointLightHelper(light);
+
+// Add daydream controller
+  $('#button').on( 'click', function () {
+    console.log("Initialise daydream");
+    var controller = new DaydreamController();
+    controller.onStateChange( function ( state ) {
+      if ( camera !== undefined ) {
+        var angle = Math.sqrt( state.xOri * state.xOri + state.yOri * state.yOri + state.zOri * state.zOri );
+        if ( angle > 0 ) {
+          axis.set( state.xOri, state.yOri, state.zOri )
+          axis.multiplyScalar( 1 / angle );
+          quaternion.setFromAxisAngle( axis, angle );
+          if ( initialised === false ) {
+            quaternionHome.copy( quaternion );
+            quaternionHome.inverse();
+            initialised = true;
+          }
+        } else {
+          quaternion.set( 0, 0, 0, 1 );
+        }
+				if (state.yTouch > 0.5){ //when down is pressed
+					console.log("down")
+					zoom +=0.05;
+					zooming();
+				}
+
+				if (state.yTouch < 0.5 && state.yTouch > 0){ //When up is pressed
+					console.log("up")
+					zoom -=0.05;
+					zooming();
+				}
+        if ( state.isHomeDown ) {
+          if ( timeout === null ) {
+            timeout = setTimeout( function () {
+              quaternionHome.copy( quaternion );
+              quaternionHome.inverse();
+            }, 1000 );
+          }
+        } else {
+          if ( timeout !== null ) {
+            clearTimeout( timeout );
+            timeout = null;
+          }
+        }
+        camera.quaternion.copy( quaternionHome );
+        camera.quaternion.multiply( quaternion );
+        // touch.position.x = ( state.xTouch * 2 - 1 ) / 1000;
+        // touch.position.y = - ( state.yTouch * 2 - 1 ) / 1000;
+        // touch.visible = state.xTouch > 0 && state.yTouch > 0;
+      }
+    } );
+
+    controller.connect();
+  } );
+
+
+	// mesh = new THREE.Mesh(
+	// 	box = new THREE.BoxGeometry(1,1,10), // width, height, depth
+	// 	new THREE.MeshBasicMaterial({color:0xff4444, wireframe:false}) // Color is given in hexadecimal RGB
+	// 	// 0xff0000 is pure red, 0x00ff00 is pure green, and 0x0000ff is pure blue.
+	// 	// white would be 0xffffff and black would be 0x000000.
+	// );
+	var axesHelper = new THREE.AxesHelper( 5 );
+scene.add( axesHelper );
+
+
+
+
+								// var testArray = [];
+								// for (var i = 1; i <= 50; i++) {
+								// 	testArray.push(i);
+								// 	}
+								x = 0;
+								y = 1;
+								z= 2;
+								$.ajax({
+									 type : 'GET',
+									 dataType : 'json',
+									 async: false,
+									 url: 'data.json',
+								  success : function(data) {
+										console.log(data._embedded.artworks[1]._links.thumbnail.href)
+										for (var i =0; i<data._embedded.artworks.length; i++){
+											var map = new THREE.TextureLoader().load(data._embedded.artworks[i]._links.thumbnail.href);
+ 											var material = new THREE.SpriteMaterial( { map: map, color: 0xffffff, fog: false} );
+											var newSprite = new THREE.Sprite(material)
+
+											if(x >= 0 && y > 0){
+												x += 0.5;
+										 		y -= 0.25;
+											}
+											else if (x > 0 && y <= 0) {
+												x -= 0.5;
+												y -= 0.25;
+											}
+											else if(x >= -3 && y < 0){
+												x -= 0.5;
+												y += 0.25;
+											}
+											else if (x < 0 && y >= 0){
+												x += 0.5;
+												y += 0.25;
+											}
+											z -= 2.5
+											console.log(x,y,z)
+											newSprite.position.set(x,y,z);
+											// newSprite.position.set(5+Math.random()*(1, 5),2+Math.random()*(1,5),0+Math.random()*(1,5))
+											newSprite.receiveShadow = true;
+											newSprite.castShadow = true;
+											scene.add(newSprite)
+										}
+								  	// console.log(data._embedded.artworks[0]);
+										// var testImage = data._embedded.artworks[0]._links.thumbnail.href;
+										// console.log(testImage)
+										// var img = document.createElement("img");
+										// img.src = testImage
+										// console.log(img)
+										// document.body.appendChild(img);
+
+
+
+									}
+								})
+								// for (var i =0; i<testArray.length; i++){
+								// 	var newSprite = new THREE.Sprite(material)
+								//
+								// 	if(x >= 0 && y > 0){
+								// 		x += 1;
+								//  		y -= 0.5;
+								// 	}
+								// 	else if (x > 0 && y <= 0) {
+								// 		x -= 1;
+								// 		y -= 0.5;
+								// 	}
+								// 	else if(x >= -3 && y < 0){
+								// 		x -= 1;
+								// 		y += 0.5;
+								// 	}
+								// 	else if (x < 0 && y >= 0){
+								// 		x += 1;
+								// 		y += 0.5;
+								// 	}
+								// 	z -= 2.5
+								// 	console.log(x,y,z)
+								// 	newSprite.position.set(x,y,z);
+								// 	// newSprite.position.set(5+Math.random()*(1, 5),2+Math.random()*(1,5),0+Math.random()*(1,5))
+								// 	newSprite.receiveShadow = true;
+								// 	newSprite.castShadow = true;
+								// 	scene.add(newSprite)
+								// }
+	// Add the mesh to the scene.
+
+
+
+	// Move the camera to 0,0,-5 (the Y axis is "up")
+
+
+	// Point the camera to look at 0,0,0
+	// camera.lookAt(new THREE.Vector3(0,0,0));
+	// Alternatively, this also works:
+	// camera.lookAt(mesh.position);
+
+	// Creates the renderer with size 1280x720
+	renderer = new THREE.WebGLRenderer();
+	renderer.setSize(1280, 720);
+	// Puts the "canvas" into our HTML page.
+	document.body.appendChild(renderer.domElement);
+
+	// Begin animation
+	animate();
+
+}
+function animate(){
+	requestAnimationFrame(animate); // Tells the browser to smoothly render at 60Hz
+
+	// Rotate our mesh.
+	// mesh.rotation.x += 0.01;
+	// mesh.rotation.y += 0.02;
+
+	// Draw the scene from the perspective of the camera.
+	renderer.render(scene, camera);
+}
+
+// When the page has loaded, run init();
+window.onload = init;
